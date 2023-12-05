7da713fda8025c5a795944beb6af19da55bb5efc
Andrés Miranda <31228376+andresmiranda2000@users.noreply.github.com>
2023-12-05 16:50:56 -0500

    app.js and index.html updated
    updated

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ["the", "our"];
+let adjectives = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+let end = [".com", ".es"];
+
+window.onload = function generateDomain() {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let a = 0; a < end.length; a++) {
+          let domain = `${pronoun[i]}${adjectives[j]}${nouns[k]}${end[i]}`;
+
+          let paragraph = document.createElement("p");
+          paragraph.textContent = domain;
+          document.getElementById("domain").appendChild(paragraph);
+        }
+      }
+    }
+  }
 };
